4b70626ee7224396f4fbb0ee7fa785f76c50879c
Mignote Tadesse <mignotetadesse@usc-guestwireless-upc-newsc7117.usc.edu>
2019-07-29 11:52:12 -0700

    Updated Lab12.py

--- Lab12.py
+++ Lab12.py
@@ -6,6 +6,10 @@
 # Description: Fibonacci and Fractals
 # -------------------------------------------------
 
+
+# ------- START ATTRIBUTED CODE SECTION -------
+# Code created with the help of Elliott Saslow
+# https://blog.goodaudience.com/fractals-and-recursion-in-python-d11d87fcf9cd
 import turtle
 import time
 def draw_square():
@@ -18,6 +22,7 @@ def draw_square():
     turtle.forward(50)
     turtle.left(90)
 turtle.color('green')
-for i in range(360):
+for i in range(60):
     draw_square()
     turtle.left(i)
+# --------- END ATTRIBUTED CODE SECTION --------
